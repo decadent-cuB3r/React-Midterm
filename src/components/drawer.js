9a933dac5d083556d3f052310b6b573efc5d5836
peggy1008123 <peggy1008123@yahoo.com.tw>
2021-04-17 19:41:44 +0800

    header rwd

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Drawer, Button } from "antd";
+import {Link} from "react-router-dom"
 
 export default function Hamburger() {
   const [visible, setVisible] = useState(false);
@@ -14,19 +15,37 @@ export default function Hamburger() {
 
   return (
     <>
-      <Button type="primary" onClick={showDrawer}>
-        Open
+      <Button type="primary" className="ham-btn" onClick={showDrawer}>
+     Menu
       </Button>
       <Drawer
-        title="Basic Drawer"
+        title="Menu"
         placement="right"
         closable={false}
         onClose={onClose}
         visible={visible}
+        className="hamburger"
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <Link to="/" className="header-ham-text">
+          關於五吉
+          About
+        </Link>
+        <Link to="/product" className="header-ham-text">
+          產品資訊
+          Product
+        </Link>
+        <Link to className="header-ham-text">
+          最新消息
+          News
+        </Link>
+        <Link to className="header-ham-text">
+          客戶須知
+          Annouce
+        </Link>
+        <Link to className="header-ham-text">
+          聯絡我們
+          Contect
+        </Link>
       </Drawer>
     </>
   );
