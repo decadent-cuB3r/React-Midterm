b121eb02cc2b7844afb6403029cdd3198f5a6683
decadent-cuB3r <cub3r.ezra.yu@gmail.com>
2021-04-17 14:28:07 +0800

    detail page css

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,56 +1,78 @@
 import { useState } from "react";
 import { Row, Col } from "antd";
-import { Select } from "antd";
+import { Select, Descriptions, Empty } from "antd";
 import AddToCart from "./AddToCart";
 import Previous from "./previousPage";
+import TableDetailImg from "./TableDetailImg";
 
 const { Option } = Select;
 
 function ProductDetail({ product }) {
   const [qty, setQty] = useState(product.countInStock > 0 ? 1 : 0);
   const [img, setImg] = useState(product.image);
-  
+
   return (
     <div>
       <Row>
-        <Col offset="3">
+        <Col lg={{ span: 8, offset: 1 }}>
           <Previous />
         </Col>
       </Row>
       <Row gutter={[32, 32]}>
-        <Col lg={{ span: 10, offset: 1 }}>
-          <img alt="" className="product-image" src={product.image} />
-          
+        <Col lg={{ span: 8, offset: 4 }} md={{ span: 6 }}>
+          <img alt="" className="product-image" src={img} />
+
+          <Descriptions title="購買資訊">
+            <Descriptions.Item label={product.description_payment[0]}>
+              {product.description_payment[1]}, {product.description_payment[2]}
+            </Descriptions.Item>
+            <Descriptions.Item label={product.description_payment[3]}>
+              {product.description_payment[4]}, {product.description_payment[5]}
+            </Descriptions.Item>
+          </Descriptions>
         </Col>
-        <Col lg={{ span: 12 }}>
-          <div className="product-info--detail">
+        <Col lg={{ span: 8, offset: 1 }}>
+          <div>
             <h1 className="product-name product-name--large">{product.name}</h1>
-            {/* <p className="product-description">{product.description}</p> */}
+            <p className="product-description--border">
+              {product.description.map((description) => (
+                <div className="product-description">{description}</div>
+              ))}
+            </p>
             <div className="product-price-wrap">
               <p className="product-price product-price--large">
                 ${product.price}.00
               </p>
               <p className="product-status">
-                Status: {product.countInStock > 0 ? "In Stock" : "Unavailable."}
+                商品囤貨: {product.countInStock > 0 ? "尚有存貨" : "已售罄"}
               </p>
               <div className="product-qty">
-              Qty: {"   "}
-              <Select
-                defaultValue={qty}
-                className="select-style"
-                onChange={(val) => setQty(val)}
-              >
-                {[...Array(product.countInStock).keys()].map((x) => (
-                  <Option key={x + 1} value={x + 1}>
-                    {x + 1}
-                  </Option>
-                ))}
-              </Select>
-            </div>
+                Qty:
+                <Select
+                  defaultValue={qty}
+                  className="select-style"
+                  onChange={(val) => setQty(val)}
+                >
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <Option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </Option>
+                  ))}
+                </Select>
+              </div>
               <p className="product-qty">Total Price: ${product.price * qty}</p>
-              <AddToCart />
             </div>
+            <AddToCart />
           </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={{ span: 10, offset: 7 }}>
+          {product.isTable === true ? (
+            <TableDetailImg />
+          ) : (
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+          )}
         </Col>
       </Row>
     </div>
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -4,15 +4,15 @@ import AddToCart from './AddToCart'
 
 export default function ProductItem({ product }) {
   return (
-    <Card className="bg-gray product">
+    <Card className="bg-gray productItem">
       <Link to={`/detail/${product.id}`}>
         <img style={{ width: "100%" }} src={product.image} alt={product.name} />
       </Link>
-      <div className="product-info">
-        <h2 className="product-name">{product.name}</h2>
+      <div className="productItem-info">
+        <h2 className="productItem-name">{product.name}</h2>
         <span className="text-gray">NTD {product.price}</span>
       </div>
-      <div className="product-functions">
+      <div className="productItem-functions">
         <AddToCart />
         <Link to={`/detail/${product.id}`}>
           <Button type="ghost">More</Button>
